a5e58030a1aa0eaaaf220b4a458fedce6e78062f
Brett Dale <brettdale828@gmail.com>
2021-11-28 02:40:56 -0500

    First launch

--- pages/index.js
+++ pages/index.js
@@ -12,11 +12,7 @@ export default function Home({ allPostsData }) {
           <title>{siteTitle}</title>
         </Head>
         <section className={utilStyles.headingMd}>
-          <p>Hey Im Brett</p>
-          <p>
-            (This is a sample website - you’ll be building a site like this on{' '}
-            <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-          </p>
+          <p>Hey Im Brett! This is my attempt at a personal blog website :D</p>
         </section>
           <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
               <h2 className={utilStyles.headingLg}>Blog</h2>
--- posts/brett.md
+++ posts/brett.md
@@ -3,4 +3,4 @@ title: 'My First Post'
 date: '2021-11-28'
 ---
 
-It's 2 in the fucking morning and I'm following a tutorial on how to build a personal blog site. What has life come to.
+Wow! I made a blogging site! Ironically I don't know what to say. Stay tuned I guess lol
